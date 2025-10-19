c4253a29de1f10b98e83b362234434846ed5110f
Maanvi Chawla <mc823@duke.edu>
2025-10-19 17:53:48 -0400

    changed gpio pins

--- PIR.py
+++ PIR.py
@@ -2,23 +2,23 @@ from gpiozero import MotionSensor
 import time
 
 # Define three PIR sensors
-pir1 = MotionSensor(4)
-pir2 = MotionSensor(5)
-pir3 = MotionSensor(6)
+pir1 = MotionSensor(23)
+pir2 = MotionSensor(24)
+pir3 = MotionSensor(25)
 
-print("Motion sensors active (GPIO 4, 5, 6). Waiting for motion...")
+print("Motion sensors active (GPIO 23, 24, 25). Waiting for motion...")
 
 while True:
     if pir1.motion_detected:
-        print("Motion detected on PIR 1 (GPIO 4)")
+        print("Motion detected on PIR 1 (GPIO 23)")
         pir1.wait_for_no_motion()
 
     if pir2.motion_detected:
-        print("Motion detected on PIR 2 (GPIO 5)")
+        print("Motion detected on PIR 2 (GPIO 24)")
         pir2.wait_for_no_motion()
 
     if pir3.motion_detected:
-        print("Motion detected on PIR 3 (GPIO 6)")
+        print("Motion detected on PIR 3 (GPIO 25)")
         pir3.wait_for_no_motion()
     
     time.sleep(0.1)  # small delay prevents deque mutation error
