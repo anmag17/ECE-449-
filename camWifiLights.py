24a76a9c47351702e16d230249d4ba3cbd18857d
unknown <hannah.chen@duke.edu>
2025-11-13 11:34:31 -0500

    add cat

--- camWifiLights.py
+++ camWifiLights.py
@@ -89,7 +89,7 @@ def run_yolo_and_save(image_path: Path):
     print(f"Annotated saved: {det_path}")
     return detected_classes
 
-TARGET_CLASSES = {"teddy bear", "groundhog", "raccoon", "squirrel"}
+TARGET_CLASSES = {"teddy bear", "groundhog", "raccoon", "squirrel", "cat", "elephant", "cow"}
 
 # Main loop
 while True:
