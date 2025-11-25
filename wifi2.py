bb57ae23d310230998a5fb1b8db539a5696897b0
unknown <hannah.chen@duke.edu>
2025-11-25 12:06:00 -0500

    just wifi testing for new wifi

--- wifi2.py
+++ wifi2.py
@@ -3,8 +3,8 @@ import socket
 def sendWiFi():
     for i in range(0, 256):
         ESP_IP = f"192.168.68.{i}"
-        PORT = 5005
-        cmd = "D3"
+        PORT = 4210
+        cmd = "DETER"
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(cmd.encode(), (ESP_IP, PORT))
         sock.close()
