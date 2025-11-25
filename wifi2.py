78e17f40daae0d6f68a01a9ed962a911594e5a85
unknown <hannah.chen@duke.edu>
2025-11-25 12:39:59 -0500

    trouble shoot wifi

--- wifi2.py
+++ wifi2.py
@@ -1,10 +1,12 @@
 import socket
+import sys
 
 def sendWiFi():
     for i in range(0, 256):
-        ESP_IP = f"192.168.68.{i}"
+        ESP_IP = f"192.168.68.{i}" 
         PORT = 5005
-        cmd = "DETER"
+        cmd = sys.argv[1] if len(sys.argv)  > 1 else "D3"
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto(cmd.encode(), (ESP_IP, PORT))
         sock.close()
